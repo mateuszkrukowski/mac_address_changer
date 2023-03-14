33ef32e162f637c988e604c4c754dff0e9aeea17
mateuszkrukowski <57400658+mateuszkrukowski@users.noreply.github.com>
2023-03-14 19:24:40 +0100

    Update mac_changer.py
    Refactored code.

--- mac_changer.py
+++ mac_changer.py
@@ -1,27 +1,46 @@
 import subprocess
-import random 
+import random
 
-prefixes = ['00:60:17:','00:60:18:','00:60:19:','00:60:1A:','00:60:1B:','00:60:1C:','00:60:1D:','00:60:1E:','00:60:1F:','00:60:20:','00:60:21:','00:60:22:','00:60:23:','00:60:24:','00:60:25:','00:60:26w:']
-digits = [0,1,2,3,4,5,6,7,8,9]
-new_mac_address=[]
-small_mac=[]
-w=''
+prefixes = [
+    '00:60:17:',
+    '00:60:18:',
+    '00:60:19:',
+    '00:60:1A:',
+    '00:60:1B:',
+    '00:60:1C:',
+    '00:60:1D:',
+    '00:60:1E:',
+    '00:60:1F:',
+    '00:60:20:',
+    '00:60:21:',
+    '00:60:22:',
+    '00:60:23:',
+    '00:60:24:',
+    '00:60:25:',
+    '00:60:26w:',
+]
+digits = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+new_mac_address = []
+identification_number = []
+random_number = ''
 
 for i in range(3):
     for y in range(2):
-        w = str((digits[random.randint(0,(len(digits)-1))]))
-        small_mac.append(w)
-    new_mac_address.append(''.join(small_mac))
-    small_mac.clear()
+        random_number = str((digits[random.randint(0, (len(digits) - 1))]))
+        identification_number.append(random_number)
+    new_mac_address.append(''.join(identification_number))
+    identification_number.clear()
 
-s = ':'.join(new_mac_address)
+formatted_new_mac_address = ':'.join(new_mac_address)
 random.shuffle(prefixes)
-d = prefixes[random.randint(0,(len(prefixes)-1))]
-s = str(d)+s
+formatted_prefix = prefixes[random.randint(0, (len(prefixes) - 1))]
+formatted_new_mac_address = str(formatted_prefix) + formatted_new_mac_address
 
 subprocess.run('ifconfig')
 internet_interface = input('Which inferace address should be changed ? ')
-subprocess.call(["sudo","ifconfig",str(internet_interface),"down"])
-subprocess.call(["sudo","ifconfig",str(internet_interface),"hw","ether",str(s)])
-subprocess.call(["sudo","ifconfig",str(internet_interface),"up"])
-subprocess.call(["ifconfig",str(internet_interface)]) 
+subprocess.call(["sudo", "ifconfig", str(internet_interface), "down"])
+subprocess.call(
+    ["sudo", "ifconfig", str(internet_interface), "hw", "ether", str(formatted_new_mac_address)]
+)
+subprocess.call(["sudo", "ifconfig", str(internet_interface), "up"])
+subprocess.call(["ifconfig", str(internet_interface)])
